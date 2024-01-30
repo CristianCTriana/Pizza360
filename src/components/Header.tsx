2dc86a0212ed0e0daf753f1f55c4004aa8c02469
Cristian Triana <cristianctr97@gmail.com>
2024-01-29 19:09:27 -0500

    estructure

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <div className="flex w-screen justify-center flex-wrap pb-4 pt-4">Logo</div>
+  );
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="flex w-screen justify-center flex-wrap pb-4 pt-4">
+      <p className="w-screen text-center">TEL: +57 1234567890</p>
+      <p className="w-screen text-center">WhatsaApp: +57 1234567890</p>
+      <p className="w-screen text-center">Dirección: Cra 00 # 12 - 34</p>
+    </div>
+  );
+}
