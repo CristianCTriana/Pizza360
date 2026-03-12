b44cafc6c643a66c7e21b4909ef71b51248608f6
CamiloW11 <cristianctr97@gmail.com>
2026-03-11 23:00:06 -0500

    menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,8 @@ import Cuadro from "@/components/cuadroitem";
 import Footer from "@/components/footer";
 import Header from "@/components/Header";
 import { useEffect, useState } from "react";
-import {
-  collection,
-  addDoc,
-  getDocs,
-} from "firebase/firestore";
-import { auth, db } from "@/lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/lib/firebase";
 
 export interface Producto {
   id?: string;
@@ -22,31 +18,64 @@ export interface Producto {
 }
 
 export default function Home() {
-  const [items, setItems] = useState<Producto[]>([]);
+  const [productos, setProductos] = useState<Producto[]>(
+    [],
+  );
 
   const productosRef = collection(db, "productos");
-
-  const obtenerProductos = async () => {
-    try {
-      const data = await getDocs(productosRef);
-      const lista = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      })) as Producto[];
-      setItems(lista);
-    } catch (error) {
-      console.error("Error obteniendo productos:", error);
-    }
-  };
+  const [cargando, setCargando] = useState(true);
 
   useEffect(() => {
-    obtenerProductos();
+    const obtenerMenu = async () => {
+      try {
+        const productosRef = collection(db, "productos");
+        const data = await getDocs(productosRef);
+
+        const lista = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as Producto[];
+
+        // EL FILTRO MÁGICO: Solo guardamos los que están disponibles
+        const platosDisponibles = lista.filter(
+          (plato) => plato.disponible === true,
+        );
+
+        setProductos(platosDisponibles);
+      } catch (error) {
+        console.error("Error obteniendo el menú:", error);
+      } finally {
+        setCargando(false); // Apagamos el estado de carga al terminar
+      }
+    };
+
+    obtenerMenu();
   }, []);
+
+  const menuAgrupado = productos.reduce(
+    (acc, producto) => {
+      if (!acc[producto.categoria])
+        acc[producto.categoria] = [];
+      acc[producto.categoria].push(producto);
+      return acc;
+    },
+    {} as Record<string, Producto[]>,
+  );
+
+  if (cargando) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <p className="text-xl text-orange-500 font-semibold animate-pulse">
+          Preparando el menú...
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <Header />
-      {items?.map((i, index) => {
+      {productos?.map((i, index) => {
         return (
           <Cuadro
             cuadrito={i.imagenUrl}
